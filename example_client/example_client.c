219405a1af7e51cec6c60193a4a6556a9a1ef9ee
Gabriel Rosenhouse <grosenhouse@vmware.com>
2021-09-26 23:15:10 -0700

    update example

--- example_client/example_client.c
+++ example_client/example_client.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include "libkv_ipc_client.h"
 
-int main() {
-   KVConn kvconn;
-   int rc = open_kvconn("some-name", &kvconn);
-   printf("c program got rc=%d\n", rc);
-   printf("c program set fd=%d\n", kvconn.fd);
+int main (int argc, char *argv[]){
+   if (argc <= 1) {
+      fprintf(stderr, "usage: %s <socket path>\n", argv[0]);
+      exit(1);
+   }
+
+   KVClient client;
+   int rc = kvclient_open(argv[1], &client);
+   if (rc != 0) {
+      fprintf(stderr, "open error: %d\n", rc);
+      exit(1);
+   }
+
+   printf("c program set fd=%d\n", client.fd);
 }
