3e8ee720907b87b31153b17e4cd265c7f1f426c9
Gabriel Rosenhouse <grosenhouse@vmware.com>
2021-09-26 21:43:15 -0700

    client

--- include/libkv_ipc_client.h
+++ include/libkv_ipc_client.h
@@ -3,25 +3,39 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-// Connection to the Key/Value Server
+// Connection to the Key/Value server
 //
 // Fields are published so the caller can allocate memory of the correct size
 // but the caller should not use the fields themselves
 typedef struct {
-  int32_t fd;
-} KVConn;
+  int fd;
+} KVClient;
+
+// Record that may be inserted into the server
+typedef struct {
+  uint32_t table_id;
+  uintptr_t key_size;
+  const uint8_t *key_data;
+  uintptr_t value_size;
+  const uint8_t *value_data;
+} KVClientRecord;
 
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
 // Create a connection to the named Key/Value server
-// Keyed off a file descriptor
-// So it is safe for use by concurrent threads and for
-// use by child processes
 //
-// Returns 0 on success or an OS errno
-int32_t open_kvconn(const char *ipc_server_name, KVConn *conn_out);
+// Keyed off a file descriptor
+// So it is safe for use by concurrent threads and for use by child processes
+//
+// Returns 0 on success
+int kvclient_open(const char *path, KVClient *kvclient_out);
+
+// Insert a record into the server
+//
+// Returns 0 on success
+int kvclient_insert(KVClient kvclient, const KVClientRecord *record);
 
 #ifdef __cplusplus
 } // extern "C"
--- src/ipc_client.rs
+++ src/ipc_client.rs
@@ -1,27 +1,135 @@
-use ::std::os::raw::c_char;
-use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::os::raw::c_int;
+use std::os::unix::io::IntoRawFd;
+use std::os::unix::net::UnixDatagram;
+use std::path::Path;
+use std::slice::from_raw_parts;
+use std::{ffi::CStr, os::unix::prelude::FromRawFd};
+
+use serde::{Deserialize, Serialize};
+
+type Result = ::std::result::Result<(), Box<dyn ::std::error::Error>>;
+type IOResult<T> = std::io::Result<T>;
+
+/// Record that may be inserted into the server
+#[repr(C)]
+pub struct KVClientRecord {
+    pub table_id: u32,
+    pub key_size: usize,
+    pub key_data: *const u8,
+    pub value_size: usize,
+    pub value_data: *const u8,
+}
 
 /// Connection to the Key/Value server
 ///
 /// Fields are published so the caller can allocate memory of the correct size
 /// but the caller should not use the fields themselves
-#[derive(Copy, Clone)]
 #[repr(C)]
-pub struct KVConn {
-    pub fd: i32,
+pub struct KVClient {
+    fd: c_int,
 }
 
 /// Create a connection to the named Key/Value server
-/// Keyed off a file descriptor
-/// So it is safe for use by concurrent threads and for
-/// use by child processes
 ///
-/// Returns 0 on success or an OS errno
+/// Keyed off a file descriptor
+/// So it is safe for use by concurrent threads and for use by child processes
+///
+/// Returns 0 on success
 #[no_mangle]
-pub extern "C" fn open_kvconn(ipc_server_name: *const c_char, conn_out: &mut KVConn) -> i32 {
-    let ipc_name = unsafe { CStr::from_ptr(ipc_server_name) }.to_str().unwrap();
-    eprintln!("ipc_client: connect({})", ipc_name);
-    conn_out.fd = -1;
-    // get the Unix Domain fd here, stash it on the KVConn
-    42
+pub extern "C" fn kvclient_open(path: *const c_char, kvclient_out: &mut KVClient) -> c_int {
+    let path = unsafe { CStr::from_ptr(path) }.to_str().unwrap();
+    eprintln!("libkv_ipc_client: kvclient_open: {}", path);
+    match KVClient::connect(path) {
+        Ok(kvclient) => {
+            *kvclient_out = kvclient;
+            return 0;
+        }
+        Err(err) => {
+            eprintln!("libkv_ipc_client: kvclient_open: error: {}", err);
+            return err.raw_os_error().unwrap_or(-1);
+        }
+    }
+}
+
+/// Insert a record into the server
+///
+/// Returns 0 on success
+#[no_mangle]
+pub extern "C" fn kvclient_insert(kvclient: KVClient, record: &KVClientRecord) -> c_int {
+    eprintln!("libkv_ipc_client: kvclient_insert");
+
+    let record = SerializableRecord {
+        table_id: record.table_id,
+        key: unsafe { from_raw_parts(record.key_data, record.key_size) },
+        value: unsafe { from_raw_parts(record.value_data, record.value_size) },
+    };
+    match kvclient.insert(&record) {
+        Ok(()) => {
+            return 0;
+        }
+        Err(err) => {
+            eprintln!("libkv_ipc_client: kvclient_insert: error: {}", err);
+            return -1;
+        }
+    }
+}
+
+// internal representation of the record
+#[derive(Serialize, Deserialize)]
+pub struct SerializableRecord<'a> {
+    pub table_id: u32,
+    pub key: &'a [u8],
+    pub value: &'a [u8],
+}
+
+impl KVClient {
+    pub fn connect<P: AsRef<Path>>(path: P) -> IOResult<Self> {
+        let ud = UnixDatagram::bind(path)?;
+        Ok(KVClient {
+            fd: ud.into_raw_fd(),
+        })
+    }
+
+    pub fn insert(self, record: &SerializableRecord) -> Result {
+        let buf = rmp_serde::to_vec(record)?;
+
+        // move fd into ud
+        let ud = unsafe { UnixDatagram::from_raw_fd(self.fd) };
+
+        // do the thing
+        let res = ud.send(&buf);
+
+        // move fd back out of ud, so it doesn't get dropped
+        let _ = ud.into_raw_fd();
+
+        res?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::os::unix::prelude::AsRawFd;
+
+    #[test]
+    fn roundtrip_ud_via_fd() -> Result {
+        let ud = UnixDatagram::unbound()?;
+
+        let orig_fd = ud.as_raw_fd();
+
+        // move fd out of ud
+        let new_owning_fd = ud.into_raw_fd();
+
+        // resurrect ud
+        let ud2 = unsafe { UnixDatagram::from_raw_fd(new_owning_fd) };
+
+        // move fd back out of ud, so it doesn't get dropped
+        let final_fd = ud2.into_raw_fd();
+
+        assert_eq!(orig_fd, final_fd);
+
+        Ok(())
+    }
 }
